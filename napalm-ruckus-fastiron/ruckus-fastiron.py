357c54ea4123432a88e4104b855342e1b79a5373
Static0verride <ohmnoo@gmail.com>
2018-04-23 10:06:21 -0700

    version in setup

--- napalm-ruckus-fastiron/ruckus-fastiron.py
+++ napalm-ruckus-fastiron/ruckus-fastiron.py
@@ -12,8 +12,6 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
-# Ruckus Napalm Version 1.0.2
-
 # Python3 support
 from __future__ import print_function
 from __future__ import unicode_literals
