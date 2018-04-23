58aeb9a84a229812c5c2cd5b0dd3007eba71d0a6
Static0verride <ohmnoo@gmail.com>
2018-04-23 10:07:33 -0700

    setup change for pip

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="napalm-ruckus-fastiron",
-    version="1.0.2",
+    version="1.0.2a",
     packages=find_packages(),
     author="Jesus Mendez",
     author_email="mendezj@staticoverride.us",
