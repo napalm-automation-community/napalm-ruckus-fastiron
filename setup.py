99b655ce82eca2040319a78ce7cf79fff90338ff
Static0verride <ohmnoo@gmail.com>
2018-04-28 15:17:02 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,20 +3,16 @@
 import uuid
 
 from setuptools import setup, find_packages
-
-try:                    # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:     # for pip <= 9.0.3
-    from pip.req import parse_requirements
+from os import path
 
 __author__ = 'Jesus Mendez <mendezj@staticoverride.us>'
 
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(ir.req) for ir in install_reqs]
+with open("requirements.txt", "r") as fs:
+    reqs = [r for r in fs.read().splitlines()]
 
 setup(
     name="napalm-ruckus-fastiron",
-    version="1.0.21",
+    version="1.0.26",
     packages=find_packages(),
     author="Jesus Mendez",
     author_email="mendezj@staticoverride.us",
