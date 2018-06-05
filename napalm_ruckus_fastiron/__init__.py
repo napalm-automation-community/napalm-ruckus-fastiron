a50521748a215bc7f1290dbbdb253eed0b4efc4f
Static0verride <ohmnoo@gmail.com>
2018-06-05 00:11:03 -0700

    Update __init__.py

--- napalm_ruckus_fastiron/__init__.py
+++ napalm_ruckus_fastiron/__init__.py
@@ -14,3 +14,10 @@
 
 """napalm-ruckus-fastiron package."""
 from napalm_ruckus_fastiron.FastIron import FastIronDriver
+
+try:
+    __version__ = pkg_resources.get_distribution('napalm-ruckus-fastiron').version
+except pkg_resources.DistributionNotFound:
+    __version__ = "Not installed"
+
+__all__ = ["FastIronDriver"]
